f82c92b81f5800ec3064677701b7c229e172e35f
AndersZobbeSoftwareUdvikling <162577923+AndersZobbeSoftwareUdvikling@users.noreply.github.com>
2024-05-23 15:54:57 +0200

    configured cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,10 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://casa.kubelab.dk/",
+    optionsSuccessStatus: 200
+}));
 app.use('/', routes());
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
