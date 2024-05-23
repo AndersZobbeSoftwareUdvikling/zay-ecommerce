19290ac6f045acdb0a62e01ced95038ddbc5ed58
AndersZobbeSoftwareUdvikling <162577923+AndersZobbeSoftwareUdvikling@users.noreply.github.com>
2024-05-23 16:03:24 +0200

    Removed /

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://casa.kubelab.dk/",
+    origin: "https://casa.kubelab.dk",
     optionsSuccessStatus: 200
 }));
 app.use('/', routes());
