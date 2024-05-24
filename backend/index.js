e1c8669466ecda5a2331543af5e4fdd0ddd7e0fc
AndersZobbeSoftwareUdvikling <162577923+AndersZobbeSoftwareUdvikling@users.noreply.github.com>
2024-05-24 10:42:06 +0200

    hardcoded things again

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_CORS,
+    origin: "https://casa.kubelab.dk",
     optionsSuccessStatus: 200
 }));
 app.use('/', routes());
